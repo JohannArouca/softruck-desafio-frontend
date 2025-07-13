0c4204478a07b440c68b4bb077dde45994b42cb4
Johann Arouca <aroucajohann@yahoo.com>
2025-07-13 16:41:57 -0300

    criacao dos testes unitarios do home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,23 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MapComponent } from './components/map/map.component';
+import { MatOptionModule } from '@angular/material/core';
 
 @NgModule({ declarations: [
         AppComponent,
-        HomeComponent
+        HomeComponent,
+        MapComponent
     ],
     bootstrap: [AppComponent], imports: [BrowserModule,
         BrowserAnimationsModule,
         ReactiveFormsModule,
+        FormsModule,
         MatFormFieldModule,
         MatSelectModule,
         AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi()), provideAnimationsAsync()] })
--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,17 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { ActivatedRoute } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatOptionModule } from '@angular/material/core';
 
-describe('HomeComponent', () => {
+fdescribe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
-    })
-    .compileComponents();
+      declarations: [HomeComponent],
+      imports: [
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        MatSelectModule
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: {
+                gpsData: { num_courses: 3 }
+              }
+            }
+          }
+        }
+      ]
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +43,13 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize courses array based on resolver', () => {
+    expect(component.courses).toEqual([1, 2, 3]);
+  });
+
+  it('should initialize selectedCourse with 0', () => {
+    expect(component.selectedCourse.value).toBe(0);
   });
 });
