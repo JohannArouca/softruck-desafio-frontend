e6b28f235bb6cfc5003478bf45abaa598f1db29f
Johann Arouca <aroucajohann@yahoo.com>
2025-07-13 17:11:20 -0300

    funcao para desenhar o trajeto selecionado

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import * as L from 'leaflet';
 import { GpsData } from 'src/app/models/gps.model';
@@ -8,24 +8,40 @@ import { GpsData } from 'src/app/models/gps.model';
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss'
 })
-export class MapComponent {
+export class MapComponent implements OnInit, OnChanges {
+  @Input() selectedCourse!: number;
   private map!: L.Map;
-    gpsData!: GpsData;
+  private currentPolyline: L.Polyline | null = null;
+  gpsData!: GpsData;
   
-    constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute) { }
   
-    ngOnInit(): void {
-      this.gpsData = this.activatedRoute.snapshot.data['gpsData'];
+  ngOnInit(): void {
+    this.gpsData = this.activatedRoute.snapshot.data['gpsData'];
+    
+    this.initMap();
+  }
   
-      this.initMap();
-    }
+  ngOnChanges(): void {
+    if(this.selectedCourse > 0) this.initCourse();
+  }
   
-    private initMap(): void {
-      this.map = L.map('map', {
-        center: [-23.963214, -46.28054],
-        zoom: 13
-      });
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [-23.963214, -46.28054],
+      zoom: 13
+    });
   
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
-    }
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
+  }
+
+  private initCourse() {
+    if (this.currentPolyline) this.map.removeLayer(this.currentPolyline);
+
+    const course = this.gpsData.courses[this.selectedCourse - 1];
+    const latlngs = course.gps.map((point: any) => [point.latitude, point.longitude]);
+
+    this.currentPolyline = L.polyline(latlngs, { color: 'blue', weight: 4 }).addTo(this.map);
+    this.map.fitBounds(this.currentPolyline.getBounds());
+  }
 }
