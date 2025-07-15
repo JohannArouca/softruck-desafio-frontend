37a8547546ac66969d8c01223dfc221f4c647f73
Johann Arouca <aroucajohann@yahoo.com>
2025-07-15 17:15:56 -0300

    ajuste na direcao do carro

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -54,24 +54,25 @@ export class MapComponent implements OnInit, OnChanges, OnDestroy {
     if (this.currentPolyline) this.map.removeLayer(this.currentPolyline);
     if (this.carMarker) this.map.removeLayer(this.carMarker);
     if (this.requestAnimationFrameId) cancelAnimationFrame(this.requestAnimationFrameId);
-
+  
     const course = this.gpsData.courses[this.selectedCourseIndex - 1];
     const latlngs = course.gps.map((p: GpsPoint) => [p.latitude, p.longitude]);
     this.currentPolyline = L.polyline(latlngs as L.LatLngExpression[], { color: 'blue' }).addTo(this.map);
     this.map.fitBounds(this.currentPolyline.getBounds());
-
-    const carIcon = L.icon({
-      iconUrl: 'assets/images/car.png',
-      iconSize: [22, 38],
-      iconAnchor: [25, 25]
+  
+    const carIcon = L.divIcon({
+      html: `<div class="car-sprite"></div>`,
+      className: '',
+      iconSize: [30, 30],
+      iconAnchor: [15, 15]
     });
-
+  
     this.carMarker = L.marker(latlngs[0] as L.LatLngTuple, { icon: carIcon }).addTo(this.map);
-
+  
     this.animationCurrentPointIndex = 0;
     this.animateStep(course.gps);
   }
-
+  
   private animateStep(gpsPoints: GpsPoint[]): void {
     if (this.animationCurrentPointIndex >= gpsPoints.length - 1) return;
 
@@ -86,15 +87,30 @@ export class MapComponent implements OnInit, OnChanges, OnDestroy {
     const now = performance.now();
     const pastTime = now - this.startTimeAnimationPoint;
     const animationTimeUnix = this.animationToPoint.acquisition_time_unix - this.animationFromPoint.acquisition_time_unix;
-    const progress = pastTime / (animationTimeUnix * 100);
-
+    const progress = pastTime / (animationTimeUnix * 50);
+  
     const latitude = this.animationFromPoint.latitude + progress * (this.animationToPoint.latitude - this.animationFromPoint.latitude);
     const longitude = this.animationFromPoint.longitude + progress * (this.animationToPoint.longitude - this.animationFromPoint.longitude);
-
+  
     this.carMarker.setLatLng([latitude, longitude]);
+  
+    const iconElement = this.carMarker.getElement() as HTMLElement;
+    const innerDiv = iconElement?.querySelector('div') as HTMLElement;
+  
+    if (innerDiv) {
+      const carFrameWidth = 30;
+      const spriteTotalCarFrames = 120;
+      const gpsDirection = this.animationToPoint.direction;
+      const spriteAngle = (360 - gpsDirection) % 360;
+      const carFrameIndex = Math.round(spriteAngle / (360 / spriteTotalCarFrames)) % spriteTotalCarFrames;
 
+      const offsetX = -(carFrameIndex * carFrameWidth);
+
+      innerDiv.style.backgroundPosition = `${offsetX}px 0`;
+    }
+  
     this.map.panTo([latitude, longitude], { animate: true });
-
+  
     if (progress < 1) {
       this.requestAnimationFrameId = requestAnimationFrame(() => this.step(gpsPoints));
     } else {
@@ -102,4 +118,5 @@ export class MapComponent implements OnInit, OnChanges, OnDestroy {
       this.animateStep(gpsPoints);
     }
   }
+  
 }
